e211ce829c9f3e41d4ba300cbab2f8128b699311
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-25 10:48:56 +0000

    Updating to latest FlutterFlow output.

--- lib/water_payment_view/water_payment_setting_page/water_payment_setting_page_model.dart
+++ lib/water_payment_view/water_payment_setting_page/water_payment_setting_page_model.dart
@@ -3,6 +3,7 @@ import '/backend/backend.dart';
 import '/backend/firebase_storage/storage.dart';
 import '/component_view/menu_toggle_view/menu_toggle_view_widget.dart';
 import '/component_view/menu_view/menu_view_widget.dart';
+import '/flutter_flow/flutter_flow_expanded_image_view.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -15,6 +16,7 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
 class WaterPaymentSettingPageModel
