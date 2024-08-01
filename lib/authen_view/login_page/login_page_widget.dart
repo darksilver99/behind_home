b62dad3fe3bc324e9ff0f4a51db33cfede0f8700
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-01 08:04:09 +0000

    Updating to latest FlutterFlow output.

--- lib/authen_view/login_page/login_page_widget.dart
+++ lib/authen_view/login_page/login_page_widget.dart
@@ -86,7 +86,7 @@ class _LoginPageWidgetState extends State<LoginPageWidget> {
                           mainAxisSize: MainAxisSize.max,
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Expanded(
+                            Flexible(
                               child: Padding(
                                 padding: EdgeInsetsDirectional.fromSTEB(
                                     16.0, 0.0, 16.0, 0.0),
