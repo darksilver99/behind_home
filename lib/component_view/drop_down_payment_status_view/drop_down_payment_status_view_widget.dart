987ec6a7ce6e2742252019de90bd6bce9fb2c1e2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-02 08:17:17 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/component_view/drop_down_payment_status_view/drop_down_payment_status_view_widget.dart
@@ -0,0 +1,173 @@
+import '/flutter_flow/flutter_flow_drop_down.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'drop_down_payment_status_view_model.dart';
+export 'drop_down_payment_status_view_model.dart';
+
+class DropDownPaymentStatusViewWidget extends StatefulWidget {
+  const DropDownPaymentStatusViewWidget({super.key});
+
+  @override
+  State<DropDownPaymentStatusViewWidget> createState() =>
+      _DropDownPaymentStatusViewWidgetState();
+}
+
+class _DropDownPaymentStatusViewWidgetState
+    extends State<DropDownPaymentStatusViewWidget> {
+  late DropDownPaymentStatusViewModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => DropDownPaymentStatusViewModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
+    return Align(
+      alignment: AlignmentDirectional(0.0, 0.0),
+      child: Container(
+        width: 300.0,
+        decoration: BoxDecoration(
+          color: FlutterFlowTheme.of(context).secondaryBackground,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Form(
+          key: _model.formKey,
+          autovalidateMode: AutovalidateMode.disabled,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 8.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        InkWell(
+                          splashColor: Colors.transparent,
+                          focusColor: Colors.transparent,
+                          hoverColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          onTap: () async {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            Icons.close_rounded,
+                            color: FlutterFlowTheme.of(context).secondaryText,
+                            size: 24.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 8.0),
+                    child: FlutterFlowDropDown<int>(
+                      controller: _model.dropDownValueController ??=
+                          FormFieldController<int>(
+                        _model.dropDownValue ??= 0,
+                      ),
+                      options: List<int>.from(FFAppState()
+                          .paymentStatusList
+                          .map((e) => e.status)
+                          .toList()),
+                      optionLabels: FFAppState()
+                          .paymentStatusList
+                          .map((e) => e.subject)
+                          .toList(),
+                      onChanged: (val) =>
+                          setState(() => _model.dropDownValue = val),
+                      width: 300.0,
+                      height: 56.0,
+                      textStyle:
+                          FlutterFlowTheme.of(context).bodyMedium.override(
+                                fontFamily: 'Manrope',
+                                letterSpacing: 0.0,
+                              ),
+                      hintText: 'Please select...',
+                      icon: Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                        color: FlutterFlowTheme.of(context).secondaryText,
+                        size: 24.0,
+                      ),
+                      fillColor:
+                          FlutterFlowTheme.of(context).secondaryBackground,
+                      elevation: 2.0,
+                      borderColor: FlutterFlowTheme.of(context).alternate,
+                      borderWidth: 2.0,
+                      borderRadius: 8.0,
+                      margin:
+                          EdgeInsetsDirectional.fromSTEB(16.0, 4.0, 16.0, 4.0),
+                      hidesUnderline: true,
+                      isOverButton: true,
+                      isSearchable: false,
+                      isMultiSelect: false,
+                    ),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      FFButtonWidget(
+                        onPressed: () async {
+                          Navigator.pop(context, _model.dropDownValue);
+                        },
+                        text: 'ตกลง',
+                        options: FFButtonOptions(
+                          height: 40.0,
+                          padding: EdgeInsetsDirectional.fromSTEB(
+                              24.0, 0.0, 24.0, 0.0),
+                          iconPadding: EdgeInsetsDirectional.fromSTEB(
+                              0.0, 0.0, 0.0, 0.0),
+                          color: FlutterFlowTheme.of(context).primary,
+                          textStyle:
+                              FlutterFlowTheme.of(context).titleSmall.override(
+                                    fontFamily: 'Manrope',
+                                    color: Colors.white,
+                                    letterSpacing: 0.0,
+                                  ),
+                          elevation: 3.0,
+                          borderSide: BorderSide(
+                            color: Colors.transparent,
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/super_admin_view/payment_list_view/payment_list_view_model.dart
@@ -0,0 +1,50 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/component_view/drop_down_payment_status_view/drop_down_payment_status_view_widget.dart';
+import '/component_view/no_data_view/no_data_view_widget.dart';
+import '/flutter_flow/flutter_flow_data_table.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
+import 'payment_list_view_widget.dart' show PaymentListViewWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class PaymentListViewModel extends FlutterFlowModel<PaymentListViewWidget> {
+  ///  Local state fields for this component.
+
+  List<PaymentListRecord> paymentList = [];
+  void addToPaymentList(PaymentListRecord item) => paymentList.add(item);
+  void removeFromPaymentList(PaymentListRecord item) =>
+      paymentList.remove(item);
+  void removeAtIndexFromPaymentList(int index) => paymentList.removeAt(index);
+  void insertAtIndexInPaymentList(int index, PaymentListRecord item) =>
+      paymentList.insert(index, item);
+  void updatePaymentListAtIndex(
+          int index, Function(PaymentListRecord) updateFn) =>
+      paymentList[index] = updateFn(paymentList[index]);
+
+  bool isLoading = true;
+
+  ///  State fields for stateful widgets in this component.
+
+  // Stores action output result for [Firestore Query - Query a collection] action in PaymentListView widget.
+  List<PaymentListRecord>? paymetListResult;
+  // State field(s) for PaginatedDataTable widget.
+  final paginatedDataTableController =
+      FlutterFlowDataTableController<PaymentListRecord>();
+  // Stores action output result for [Alert Dialog - Custom Dialog] action in Text widget.
+  int? isUpdate;
+  // Stores action output result for [Firestore Query - Query a collection] action in Text widget.
+  List<PaymentListRecord>? paymetListResult2;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
